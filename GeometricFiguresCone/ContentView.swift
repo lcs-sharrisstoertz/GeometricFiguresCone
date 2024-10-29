877520bf63edde7bb632a1ddc84e581d645a2186
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-10-29 11:16:49 -0400

    Initial Commit

---
+++ GeometricFiguresCone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeometricFiguresCone
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometricFiguresCone/GeometricFiguresConeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresConeApp.swift
+//  GeometricFiguresCone
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresConeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
